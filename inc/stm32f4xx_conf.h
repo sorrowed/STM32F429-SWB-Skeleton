7432a31c8b0e5cd839f1666c42e5e790b2abfe9c
Tom Bulsink <github@sorrowed.nl>
2015-05-25 21:03:35 +0200

    - Add skeleton project after making it build, even after refresh. The project did not do that after i renamed it from a to this name because there was some setting that instructed the builder to refresh resource "a" after clean

---
+++ inc/stm32f4xx_conf.h
@@ -0,0 +1,16 @@
+/*
+ * stm32f4xx_conf.h
+ *
+ *  Created on: May 20, 2015
+ *      Author: tom
+ */
+
+#ifndef STM32F4XX_CONF_H_
+#define STM32F4XX_CONF_H_
+
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+
+#define assert_param(a)
+
+#endif /* STM32F4XX_CONF_H_ */
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: May 20, 2015
+ *      Author: tom
+ */
+
+#include "stm32f4xx.h"
+#include <stdbool.h>
+
+int main( void )
+{
+	while( true )
+		;
+
+	return -1;
+}
